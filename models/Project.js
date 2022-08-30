dce26ecd80bf6bfc4186cbd5251f2beaeb8787f5
nihalsangole <nihalsangole9@gmail.com>
2022-08-30 17:07:35 +0530

    2

--- models/Project.js
+++ models/Project.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const ProjectSchema = new mongoose.Schema(
   {
-    user: { type: String, required: true },
+    userId: { type: String, required: true },
     title: { type: String, required: true, unique: true },
     desc: { type: String, required: true },
-    img: { type: String },
-    categories: { type: Array },
+    img: { type: String, default: '' },
+    categories: { type: Array, default: [] },
   },
   { timestamps: true }
 );
